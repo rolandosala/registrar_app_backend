2d55a19bd9a3af737a642476c6c84a06f30c942e
Roland <rolandosalajr@gmail.com>
2025-08-27 21:10:32 +0800

    fix cors

--- local_server.js
+++ local_server.js
@@ -17,7 +17,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
